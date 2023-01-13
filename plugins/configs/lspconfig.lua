389a569c6baef9c6f3dd6dba24163f3aab3478ce
Corentin Prevost <prevost.corentin@yahoo.com>
2023-01-13 19:53:16 +0100

    Better LSP/formatting related autocommands * Defining buffer scoped autocommands when null-ls/[lsp-server] is attaching to it * `stylua` formatting

--- plugins/configs/lspconfig.lua
+++ plugins/configs/lspconfig.lua
@@ -4,56 +4,62 @@ if not present then
   return
 end
 
-lspconfig.ansiblels.setup({
+local lsp_augroup = vim.api.nvim_create_augroup("lsp", { clear = true })
+
+local on_attach_callback = function(client, bufnr)
+  vim.api.nvim_clear_autocmds { group = lsp_augroup, buffer = bufnr }
+  if true == client.server_capabilities.documentHighlightProvider then
+    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+      callback = vim.lsp.buf.document_highlight,
+      group = lsp_augroup,
+    })
+  end
+  vim.api.nvim_create_autocmd({ "CursorMoved" }, {
+    callback = vim.lsp.buf.clear_references,
+    group = lsp_augroup,
+  })
+  vim.api.nvim_create_autocmd({ "CursorHoldI" }, {
+    callback = vim.diagnostic.open_float,
+    group = lsp_augroup,
+  })
+  vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+    callback = vim.diagnostic.setloclist,
+    group = lsp_augroup,
+  })
+end
+
+lspconfig.ansiblels.setup {
+  on_attach = on_attach_callback,
   ansible = {
     ansible = {
-      path = "ansible"
+      path = "ansible",
     },
     executionEnvironment = {
-       enabled = false
+      enabled = false,
     },
     python = {
-      interpreterPath = "python"
+      interpreterPath = "python",
     },
     validation = {
       enabled = true,
       lint = {
         enabled = true,
-        path = "ansible-lint"
-      }
-    }
-  }
-})
-lspconfig.bashls.setup({})
-lspconfig.dockerls.setup({})
-lspconfig.jsonls.setup({})
-lspconfig.pyright.setup({
+        path = "ansible-lint",
+      },
+    },
+  },
+}
+lspconfig.bashls.setup { on_attach = on_attach_callback }
+lspconfig.dockerls.setup { on_attach = on_attach_callback }
+lspconfig.jsonls.setup { on_attach = on_attach_callback }
+lspconfig.pyright.setup {
+  on_attach = on_attach_callback,
   python = {
     analysis = {
       autoSearchPaths = true,
       diagnosticMode = "workspace",
-      useLibraryCodeForTypes = true
-    }
-  }
-})
-lspconfig.yamlls.setup({})
-
-local lsp = vim.api.nvim_create_augroup("lsp", { clear = true })
-
-vim.api.nvim_create_autocmd({"CursorHold", "CursorHoldI"}, {
-  pattern = {"*.bash", "*.c", "*.lua", "*.sh", "*.py", "*.zsh"},
-  callback = vim.lsp.buf.document_highlight,
-  group = lsp })
-
-vim.api.nvim_create_autocmd({"CursorMoved"}, {
-  pattern = {"*.bash", "*.c", "*.lua", "*.sh", "*.py", "*.zsh"},
-  callback = vim.lsp.buf.clear_references,
-  group = lsp })
-
-vim.api.nvim_create_autocmd({"CursorHoldI"}, {
-  callback = vim.diagnostic.open_float,
-  group = lsp })
-
-vim.api.nvim_create_autocmd({"BufWritePost"}, {
-  callback = vim.diagnostic.setloclist,
-  group = lsp })
+      useLibraryCodeForTypes = true,
+    },
+  },
+}
+lspconfig.yamlls.setup { on_attach = on_attach_callback }
--- plugins/configs/null-ls.lua
+++ plugins/configs/null-ls.lua
@@ -4,6 +4,8 @@ if not present then
   return
 end
 
+local null_ls_augroup = vim.api.nvim_create_augroup("null_ls", { clear = false })
+
 null_ls.setup {
   debug = true,
   sources = {
@@ -13,14 +15,16 @@ null_ls.setup {
     null_ls.builtins.formatting.shellharden,
     null_ls.builtins.code_actions.shellcheck,
   },
-}
-
-local null_ls_augroup = vim.api.nvim_create_augroup("null_ls", { clear = false })
-
-vim.api.nvim_create_autocmd({ "BufWrite" }, {
-  group = null_ls_augroup,
-  buffer = vim.api.nvim_get_current_buf(),
-  callback = function()
-    vim.lsp.buf.format()
+  on_attach = function(client, bufnr)
+    if true == client.server_capabilities.documentFormattingProvider then
+      vim.api.nvim_clear_autocmds { group = null_ls_augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+        group = null_ls_augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format { bufnr = bufnr }
+        end,
+      })
+    end
   end,
-})
+}
